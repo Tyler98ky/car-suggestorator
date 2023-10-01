c0922b36857c4e7df881d5813312f7950758f3d1
Tyler Howard <tyler98ky@gmail.com>
2023-10-01 16:18:14 -0700

    update prompt to be more readable

--- src/components/CarDescript.jsx
+++ src/components/CarDescript.jsx
@@ -3,12 +3,12 @@ import PropTypes from "prop-types";
 const CarDescript = (prompt, carData) => {
   const url = "https://api.openai.com/v1/chat/completions";
 
-  const sysMsg = `In a conversational professional tone, answer the [Question] based on the [Car Data]. 
+  const sysMsg = `In a conversational professional tone, answer the [Question] based on the [Car]. Return 4 bullet points answering the following concerns. 
 
-- Provide an opinion about why this car fits the prompt's needs. 
-- Provide an example of something this car does very well. 
-- Provide an alternative of other similar cars that the user should consider as well.
-- Provide a brief summary of the expected expenses related to this vehicle over the next 5 years.`;
+- Provide 1 sentence of an opinion about why this car fits the prompt's needs.
+- Provide 1 sentence of an opinion about why this car fits the prompt's needs.
+- Provide 1 sentence of an opinion about why this car fits the prompt's needs. 
+- Give 1 to 2 sentences of a breakdown of the expected monthly cost of ownership for this car. Try to provide number estimates.`;
 
   const newPrompt = `Question: ${prompt}. Car Data: ${JSON.stringify(carData)}`;
 
